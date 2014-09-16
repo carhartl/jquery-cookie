d329b4e77376939fa49290eb0c68b60cfb7aeb40
Igor Escobar <blog@igorescobar.com>
2014-09-16 13:57:46 -0300

    adding httpOnly option

--- src/jquery.cookie.js
+++ src/jquery.cookie.js
@@ -69,7 +69,8 @@
 				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
 				options.path    ? '; path=' + options.path : '',
 				options.domain  ? '; domain=' + options.domain : '',
-				options.secure  ? '; secure' : ''
+				options.secure  ? '; secure' : '',
+				options.httpOnly ? '; HttpOnly' : ''
 			].join(''));
 		}
 
